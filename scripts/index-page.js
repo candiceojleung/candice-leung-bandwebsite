24c1e3cb702bc9b9341d856ac63c83bb0dcff918
candiceojleung <candice.leung@live.ca>
2025-01-28 01:11:23 -0500

    creat shows page, update header and footer for page, and embed soundcloud track

---
+++ scripts/index-page.js
@@ -0,0 +1,23 @@
+const comments = [
+  {
+    name: "Isaac Tadesse",
+    comment:
+      "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+    date: new Date("10/20/2023"),
+  },
+  {
+    name: "Christina Cabrera",
+    comment:
+      "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
+    date: new Date("02/28/2024"),
+  },
+  {
+    name: "Victor Pinto",
+    comment:
+      "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+    date: new Date("08/14/2024"),
+  },
+];
+
+const date = new Date("08/14/2024");
+console.log(date.toLocaleDateString()); // output: '8/14/2024'
