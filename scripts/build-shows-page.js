3a66a388d213a933cb48ff8c09cff04f6b3ee670
candiceojleung <candice.leung@live.ca>
2025-01-30 13:00:05 -0500

    fix padding and alignment on shows, and added a bottom border as a divider

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -35,73 +35,55 @@ const list = document.getElementById("show-list");
 
 for (const showObj of shows) {
   const show = document.createElement("div");
-  show.classList.add ("show__item");
+  show.classList.add("show__item");
 
+  const showDate = document.createElement("div");
+  showDate.classList.add("show__header");
+  showDate.innerHTML = "Date";
+  show.append(showDate);
 
-const showDate =document.createElement("div");
-showDate.classList.add("show__header"); 
-showDate.innerHTML= "Date";
-show.append(showDate); 
+  const dateInfo = document.createElement("p");
+  dateInfo.classList.add("show__info--bold");
+  dateInfo.innerHTML = showObj.date;
+  show.append(dateInfo);
 
-const dateInfo=document.createElement("p");
-dateInfo.classList.add("show__info--bold");
-dateInfo.innerHTML=showObj.date;
-show.append(dateInfo);
+  const showVenue = document.createElement("div");
+  showVenue.classList.add("show__header");
+  showVenue.innerHTML = "Venue";
+  show.append(showVenue);
 
-const dateSpacer=document.createElement("div");
-dateSpacer.classList.add ("show__spacer"); 
-dateSpacer.innerHTML=" ";
-show.append(dateSpacer); 
+  const venueInfo = document.createElement("p");
+  venueInfo.classList.add("show__info");
+  venueInfo.innerHTML = showObj.venue;
+  show.append(venueInfo);
 
-const showVenue=document.createElement("div");
-showVenue.classList.add("show__header");
-showVenue.innerHTML= "Venue";
-show.append(showVenue);
+  const showLocation = document.createElement("div");
+  showLocation.classList.add("show__header");
+  showLocation.innerHTML = "Location";
+  show.append(showLocation);
 
-const venueInfo=document.createElement("p");
-venueInfo.classList.add("show__info");
-venueInfo.innerHTML=showObj.venue;
-show.append(venueInfo);
+  const locationInfo = document.createElement("p");
+  locationInfo.classList.add("show__info");
+  locationInfo.innerHTML = showObj.location;
+  show.append(locationInfo);
 
-const venueSpacer=document.createElement("div");
-venueSpacer.classList.add ("show__spacer"); 
-venueSpacer.innerHTML=" ";
-show.append(venueSpacer); 
+  const button = document.createElement("button");
+  button.classList.add("button");
+  button.innerHTML = "buy tickets";
+  show.append(button);
 
-const showLocation=document.createElement("div");
-showLocation.classList.add("show__header");
-showLocation.innerHTML="Location";
-show.append(showLocation);
-
-const locationInfo=document.createElement("p");
-locationInfo.classList.add("show__info"); 
-locationInfo.innerHTML=showObj.location;
-show.append(locationInfo); 
-
-const locationSpacer=document.createElement("div");
-locationSpacer.classList.add ("show__spacer"); 
-locationSpacer.innerHTML=" ";
-show.append(locationSpacer); 
-
-const button =document.createElement ("button");
-button.classList.add("button");
-button.innerHTML="buy tickets";
-show.append(button); 
-
-list.append(show);
-
-};
-
+  list.append(show);
+}
 
 list.addEventListener("click", (e) => {
   const el = e.target.closest(".show__item");
 
-if (!el) {
-  return;
-}
+  if (!el) {
+    return;
+  }
   const activeClass = "show__item--active";
   const active = list.querySelector(`.${activeClass}`);
-if (active) {
+  if (active) {
     active.classList.remove(activeClass);
   }
   el.classList.add(activeClass);
