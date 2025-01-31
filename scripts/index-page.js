05f114a6b4bb8e67aeeb7427a2a81903edcc386e
candiceojleung <candice.leung@live.ca>
2025-01-30 20:57:37 -0500

    add iframe and style according to media query requirements

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -12,17 +12,16 @@ const comments = [
       "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
   },
   {
-   
-      name: "Victor Pinto",
-      date: new Date("11/02/2023"),
-      comment:
-        "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
-  
+    name: "Victor Pinto",
+    date: new Date("11/02/2023"),
+    comment:
+      "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
   },
 ];
 
-// const date = new Date("08/14/2024");
-// console.log(date.toLocaleDateString()); // output: '8/14/2024'
+const date = new Date();
+console.log(date.toLocaleDateString())
+; // output: '8/14/2024'
 
 // const dateToday = new Date();
 // const day= dateToday.getDay();
@@ -30,49 +29,57 @@ const comments = [
 // const year=dateToday.getYear();
 // console.log(dateToday);
 
-
-
+const form = document.getElementById("formContent");
 const formComment = document.getElementById("comment-section");
 
 for (const commentObj of comments) {
   const comment = document.createElement("div");
-  comment.classList.add ("comment__item");
+  comment.classList.add("comment__item");
 
+  const avatar = document.createElement("div");
+  avatar.classList.add("comment__avatar");
+  avatar.textContent = " ";
+  comment.append(avatar);
 
-const avatar=document.createElement('div');
-avatar.classList.add ("comment__avatar");
-avatar.textContent=" ";
-comment.append(avatar);
- 
-const text =document.createElement('div');
-text.classList.add('comment__context');
-comment.append(text);
+  const text = document.createElement("div");
+  text.classList.add("comment__context");
+  comment.append(text);
 
-const info=document.createElement("div");
-info.classList.add("comment__info");
-text.append(info);
+  const info = document.createElement("div");
+  info.classList.add("comment__info");
+  text.append(info);
 
-const commentName=document.createElement("p");
-commentName.classList.add("comment__name");
-commentName.textContent= commentObj.name;
-info.append(commentName);
+  const commentName = document.createElement("p");
+  commentName.classList.add("comment__name");
+  commentName.textContent = commentObj.name;
+  info.append(commentName);
 
-const commentDate=document.createElement("p");
-commentDate.classList.add("comment__date"); 
-commentDate.textContent= commentObj.date; 
-info.append(commentDate);
+  const commentDate = document.createElement("p");
+  commentDate.classList.add("comment__date");
+  commentDate.textContent = commentObj.date;
+  info.append(commentDate);
 
-const commentText=document.createElement("p");
-commentText.classList.add("comment__text");
-commentText.textContent= commentObj.comment;
-text.append(commentText);
+  const commentText = document.createElement("p");
+  commentText.classList.add("comment__text");
+  commentText.textContent = commentObj.comment;
+  text.append(commentText);
 
-formComment.prepend(comment);
-};
+  formComment.prepend(comment);
+}
 
+// // Event listener for form submission
+// function displayComment(commentsObj) {
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
 
+//   const newComment = {
+//     name: e.target.name.value,
+//     date: e.target.date.value,
+//     comment: e.target.comment.value,
+//   };
+//   comments.push(newComment);
 
-
-// formComment.addEventListener("submit", (e) => {
-//     e.preventDefault();
+//   formComment.replaceChildren;
+//   comments.forEach(displayComment); 
+// });
 // };
